5fb328217541db30d1a264cc527d1ee7b5ebf0b9
MyCah Pleasant <mycahp@gmail.com>
2017-11-05 21:56:32 -0500

    Add static folder and static route

--- led-controller/app.py
+++ led-controller/app.py
@@ -1,4 +1,7 @@
-from bottle import Bottle
+from bottle import (
+    Bottle,
+    static_file
+)
 
 from leds import ledAPI
 from strips import stripAPI
@@ -7,7 +10,11 @@ app = Bottle()
 
 @app.route('/')
 def index():
-    return "Test"
+    return static_file("index.html", "static")
+
+@app.route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
 
 if __name__ == '__main__':
     app.merge(ledAPI)
